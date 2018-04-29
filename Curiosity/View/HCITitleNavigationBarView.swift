3c18432923b5192da93a8868237804ee9ac0f7e2
vosamoilenko <samoilenkovolodymyr@gmail.com>
2018-04-29 15:59:14 +0200

    Return ,y part

---
+++ Curiosity/View/HCITitleNavigationBarView.swift
@@ -0,0 +1,59 @@
+//
+//  HCITitleNavigationBarView.swift
+//  Curiosity
+//
+//  Created by Vo1 on 28.04.18.
+//  Copyright © 2018 InteractiveNewsExplorer. All rights reserved.
+//
+
+import UIKit
+
+
+class HCITitleNavigationBarView: UIView {
+    @IBOutlet weak var logoLabel: UILabel!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var menuButton: UIButton!
+    
+    var view: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    @objc func notify(sender: UIButton) {
+        if sender.tag == 0 {
+            NotificationCenter.default.post(Notification.init(name: Notification.Name(rawValue: "menuButtonPressed")))
+        } else if sender.tag == 1 {
+            NotificationCenter.default.post(Notification.init(name: Notification.Name(rawValue: "searchButtonPressed")))
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleHeight.rawValue) | UInt8(UIViewAutoresizing.flexibleWidth.rawValue)))
+        searchButton.addTarget(self, action: #selector(notify), for: .touchUpInside)
+        menuButton.addTarget(self, action: #selector(notify), for: .touchUpInside)
+        
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView! {
+        let bundle = Bundle.init(for: type(of: self))
+        let nib = UINib.init(nibName: "HCITitleNavigationBarView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        return view
+    }
+
+}
+
+
--- Curiosity/ViewController.swift
+++ Curiosity/ViewController.swift
@@ -9,17 +9,134 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var titleView: HCITitleNavigationBarView!
+    @IBOutlet var rightSwipeRecognizer: UISwipeGestureRecognizer!
+    @IBOutlet var leftSwipeRecognizer: UISwipeGestureRecognizer!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var topConstrain: NSLayoutConstraint!
+    
+    
+    
+    var categoryIndex = 0
+    var isSearchBarVisible = false
+    var isMenuBarVisible = false
+    var searchRequest: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.searchBar.delegate = self
+        
+        let nib = UINib(nibName: "HCINewsTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "HCINewsTableViewCell")
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44
+        
+        self.titleView.category.text = cat[categoryIndex]
+        
+        self.leftSwipeRecognizer.addTarget(self, action: #selector(handleSwipe))
+        self.rightSwipeRecognizer.addTarget(self, action: #selector(handleSwipe))
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(openSearchBar),
+            name: Notification.Name("searchButtonPressed"),
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(openMenuBar),
+            name: Notification.Name("menuButtonPressed"),
+            object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isSearchBarVisible {
+            openSearchBar()
+        }
     }
+    @objc func openMenuBar() {
+        print("check")
+    }
+    @objc func openSearchBar() {
+        var value = self.topConstrain.constant
 
-
+        if !isSearchBarVisible {
+            value += self.searchBar.frame.size.height
+            self.searchBar.becomeFirstResponder()
+        } else {
+            value -= self.searchBar.frame.size.height
+            self.searchBar.resignFirstResponder()
+        }
+        isSearchBarVisible = !isSearchBarVisible
+        self.topConstrain.constant = value
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
 }
-
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchRequest = searchText
+        print(searchRequest)
+    }
+}
+// Manage Swipe and row animations in tableView
+extension ViewController  {
+    @objc func handleSwipe(sender: UISwipeGestureRecognizer) {
+        changeCategory(direction: sender.direction)
+    }
+    func changeCategory(direction: UISwipeGestureRecognizerDirection) {
+        
+        
+        let isRight = (direction == .right) ? true : false
+        let animationDirection = isRight ? UITableViewRowAnimation.right : UITableViewRowAnimation.left
+        if isRight {
+            categoryIndex += 1
+        } else {
+            categoryIndex -= 1
+        }
+        if categoryIndex < 0 {categoryIndex = cat.count - 1}
+        self.titleView.category.text = cat[categoryIndex%6]
+        
+        var arr = [IndexPath]()
+        for var i in decr.indices {
+            arr.append(IndexPath.init(row: i, section: 0))
+        }
+        self.tableView.reloadRows(at: arr, with: animationDirection)
+    }
+}
+extension ViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if isSearchBarVisible {
+            openSearchBar()
+        }
+    }
+}
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "HCINewsTableViewCell", for: indexPath) as? HCINewsTableViewCell
+        
+        let description = decr[indexPath.row]
+        let title = tit[indexPath.row]
+        let author = auth[indexPath.row]
+        let date = dat[indexPath.row]
+        
+        cell?.descriptionLabel.text = description
+        cell?.titleLabel.text = title
+        cell?.authorLabel.text = author
+        cell?.dateLabel.text = date
+        
+        
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return decr.count;
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+}
